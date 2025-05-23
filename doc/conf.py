8af511cdb7ba3467adbdaf9e990a06b544b25f63
bpuchala <bpuchala@users.noreply.github.com>
2025-05-23 10:11:44 -0400

    Add dof symbol images; Update docs configuration

--- doc/conf.py
+++ doc/conf.py
@@ -182,19 +182,19 @@ html_theme_options = {
             "type": "fontawesome",
         },
     ],
-    "favicons": [
-        {
-            "rel": "icon",
-            "sizes": "32x32",
-            "href": "favicon-32x32.png",
-        },
-        {
-            "rel": "icon",
-            "sizes": "16x16",
-            "href": "favicon-16x16.png",
-        },
-        {"rel": "apple-touch-icon", "sizes": "180x180", "href": "apple-touch-icon.png"},
-    ],
+    # "favicons": [
+    #     {
+    #         "rel": "icon",
+    #         "sizes": "32x32",
+    #         "href": "favicon-32x32.png",
+    #     },
+    #     {
+    #         "rel": "icon",
+    #         "sizes": "16x16",
+    #         "href": "favicon-16x16.png",
+    #     },
+    #     {"rel": "apple-touch-icon", "sizes": "180x180", "href": "apple-touch-icon.png"},
+    # ],
     # "primary_sidebar_end": ["primary_sidebar_end"]
 }
 # Add any paths that contain custom static files (such as style sheets) here,
@@ -204,6 +204,7 @@ html_static_path = ["_static"]
 html_css_files = [
     "css/custom.css",
 ]
+html_favicon = "_static/favicon-16x16.png"
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
